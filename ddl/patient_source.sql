36138b08fd5c0259273d85e9f8d4bdd3a91719f2
Y sathwik abhignan <85876868+Sathwik-i9@users.noreply.github.com>
2025-11-25 17:24:05 +0530

    Create patient_source.sql

---
+++ ddl/patient_source.sql
@@ -0,0 +1,10 @@
+CREATE TABLE patient_source (
+    patient_id STRING,
+    name STRING,
+    age INT,
+    address STRING,
+    phone_number STRING,
+    bill_amount DOUBLE,
+    insurance_provider STRING,
+    policy_id STRING
+);
